f651ed7024bf27f75644581e32dcfa175ae1d00a
Jakob Emmer <j.emmer@htlkrems.at>
2025-11-18 10:42:46 +0100

    Implement constructor for Produktionsmatrix with validation and initialization logic

--- SimplexSolver/Produktionsmatrix.cs
+++ SimplexSolver/Produktionsmatrix.cs
@@ -15,8 +15,45 @@ namespace LO_bibCORE
 
         // Hilfsarray
         private double[] faktor;//quasi die absoluten Zellbezüge vom Excel
+        
+        public Produktionsmatrix(int anzProdukte, int anzNebenbed)
+        {
+            if (anzProdukte < 1) anzProdukte = 1;
+            if (anzNebenbed < 0) anzNebenbed = 0;
 
-        public Produktionsmatrix(int anzProdukte, int anzNebenbed){}
+            int rows = anzNebenbed + 1;
+            int cols = anzProdukte + 1;
+
+            matrix = new double[rows, cols];
+            schlupf = new double[rows, anzNebenbed];
+
+            for (int i = 1; i < rows; i++)
+            {
+                int slackIndex = i - 1;
+                if (slackIndex < anzNebenbed)
+                    schlupf[i, slackIndex] = 1.0;
+            }
+
+            rs = new double[rows];
+            q = new double[rows];
+
+            int totalColumns = anzProdukte + anzNebenbed + 1;
+            faktor = new double[totalColumns];
+            legenden = new string[totalColumns];
+
+            for (int i = 0; i < anzProdukte; i++)
+                legenden[i] = "x" + (i + 1);
+
+            for (int i = 0; i < anzNebenbed; i++)
+                legenden[anzProdukte + i] = "s" + (i + 1);
+
+            legenden[anzProdukte + anzNebenbed] = "b";
+
+            pivotSpalte = -1;
+            pivotZeile = -1;
+            Solved = false;
+        }
+        
         public void fillLine(int zeile, double[] werte){}
         //Pivot-Spalte ermitteln
         private int GetPivotSpalte(){}
